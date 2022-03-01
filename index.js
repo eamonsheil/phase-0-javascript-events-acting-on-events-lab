99052a279ab564c9a2b383087bf1bc3ca6f86e81
Eamon Sheil <Eamondsheil@gmail.com>
2022-03-01 11:34:21 -0500

    commit tree b0cdf692d45ed09e8ecf30b64e6a42a9fb498d68

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+const dodger = document.querySelector('#dodger')
+console.log(dodger)
+
+
+
+
+document.addEventListener('keydown', e => {
+    if (e.key === "ArrowLeft") 
+    moveDodgerLeft();
+    else if (e.key === "ArrowRight")
+    moveDodgerRight();
+    else if (e.key === "ArrowUp")
+    moveUp();
+    
+})
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+   if (left > 0)
+   dodger.style.left = `${left - 2}px`;
+
+
+} 
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+    if (right < 360)
+    dodger.style.left = `${right + 2}px`;
+}
+
+function moveUp(){
+    const topNumbers = dodger.style.bottom.replace("px", "");
+    const top = parseInt(topNumbers, 10);
+
+    dodger.style.bottom = `${top + 2}px`
+}
